91a8277ce01e4955e19f7dc8415aedecb096413e
Antoine d'Aligny <antoinedaligny@outlook.fr>
2020-06-04 14:12:08 +0200

    Add footer and header biding

---
+++ js/ComponentBinding.js
@@ -0,0 +1,12 @@
+fetch("./component/header.html")
+  .then((response) => response.text())
+  .then((data) => insertAndExecute("header", data));
+fetch("./component/footer.html")
+  .then((response) => response.text())
+  .then((data) => insertAndExecute("footer", data));
+/** Highly unsecured but its OK for this project */
+function insertAndExecute(selector, text) {
+  const element = document.querySelector(selector);
+  element.innerHTML = text;
+  nodeScriptReplace(element);
+}
